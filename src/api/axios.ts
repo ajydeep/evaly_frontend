a945e923075a97c93e458a88a7c503db61f75588
Ajaydeep <ajaydeep9966@gmail.com>
2026-02-27 10:58:20 +0530

    use env for api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api/v1',
   headers: { 'Content-Type': 'application/json' },
 })
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
